a311f09321d52753828f87027c6687a4133b7ed2
pixaal-vt <pixaal-vt@protonmail.com>
2018-07-23 18:10:17 +0200

    Fix damage not tracked

--- Less Annoying Friendly Fire/scripts/mods/Less Annoying Friendly Fire/Less Annoying Friendly Fire.lua
+++ Less Annoying Friendly Fire/scripts/mods/Less Annoying Friendly Fire/Less Annoying Friendly Fire.lua
@@ -8,7 +8,7 @@ mod:hook("PlayerUnitHealthExtension.add_damage", function (func, self, attacker_
     if DamageUtils.is_player_unit(attacker_unit) then
         if (mod:get("mode") == 3 and damage_amount < mod:get("threshold")) or mod:get("mode") == 2 then
             -- mod:echo("dmg " .. damage_type .." " .. tostring(damage_amount))  -- debug
-            damage_type = "temporary_health_degen"  -- this damage type doesn't show the direction indicator
+            damage_type = "knockdown_bleed"  -- this damage type doesn't show the direction indicator
         end
     end
 
