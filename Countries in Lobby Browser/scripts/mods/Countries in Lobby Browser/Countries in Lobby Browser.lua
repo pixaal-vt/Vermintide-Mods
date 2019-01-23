9b6b205a4cc98b838dc1f1c0cb7bb660fe4cb68a
pixaal-vt <pixaal-vt@protonmail.com>
2019-01-23 20:01:56 +0200

    Allow filtering of lobby list

--- Countries in Lobby Browser/scripts/mods/Countries in Lobby Browser/Countries in Lobby Browser.lua
+++ Countries in Lobby Browser/scripts/mods/Countries in Lobby Browser/Countries in Lobby Browser.lua
@@ -1,65 +1,50 @@
 local mod = get_mod("Countries in Lobby Browser")
 
+local filter = ""
+
 --[[
     Functions
 --]]
 
-local function starts_with(str, start)
-    return str:sub(1, #start) == start
-end
+local cmd_country_description = [[
+Filter the lobby browser with hosts from this country.
+Separate multiple countries with spaces.
+Reset by running /country with no parameters.
+]]
+
+mod:command("country", cmd_country_description, function(...)
+    filter = table.concat({...}, " ")
+    if filter == "" then
+        mod:echo("Lobby filter reset.")
+    else
+        mod:echo("Lobby filter changed, click \"Search\" to update the list. Reset the filter by running /country with no parameters.")
+    end
+end)
 
 --[[
     Hooks
 --]]
 
-mod:hook(LobbyItemsList, "populate_lobby_list", function(func, self, lobbies, ...)
+mod:hook(LobbyItemsList, "remove_invalid_lobbies", function(func, self, lobbies)
+    local filtered_lobbies = {}
+    local num_lobbies = #lobbies
 
-    local sort_func = self.sort_lobbies_function
-    local valid_lobbies = self:remove_invalid_lobbies(lobbies)
-
-    if sort_func then
-        self:sort_lobbies(valid_lobbies, sort_func)
-    end
-
-    local my_peer_id = Network:peer_id()
-    for lobby_id, lobby_data in pairs(valid_lobbies) do
-
-        ---- Semi-copy pasta from lobby_item_list.lua / create_lobby_list_entry_content to get only valid lobbies ----
-        local host = lobby_data.host
+    local i = 0
+    for lobby_id, lobby_data in pairs(lobbies) do
+        i = i + 1
         local title_text = lobby_data.server_name or lobby_data.unique_server_name or lobby_data.name or lobby_data.host
-
-        local continue = true
-        if host == my_peer_id or not title_text then
-            continue = false
-        end
-        ---- End copy pasta ----
-
-        if continue then
-            country_code_text = '['..lobby_data.country_code..'] '
-            if not starts_with(title_text, country_code_text) then
-                modded_title = country_code_text .. title_text
-                lobby_data.server_name = modded_title  -- 'server_name' is the first name that create_lobby_list_entry_content will use out of 'server_name', 'unique_server_name', 'name' and 'host'
-                -- It's safe to modify lobby_data.server_name as it is only used for display in the lobby browser
-            end
+        if string.find(filter, lobby_data.country_code) or filter == "" then
+            filtered_lobbies[#filtered_lobbies + 1] = lobbies[i]
         end
     end
 
-    func(self, lobbies, ...)
+    return func(self, filtered_lobbies)
 end)
 
-
---[[
-    Callbacks
---]]
-
--- All callbacks are called even when the mod is disabled
-
--- Called when the checkbox for this mod is unchecked
-mod.on_disabled = function(is_first_call)
-    mod:disable_all_hooks()
-end
-
--- Called when the checkbox for this is checked
-mod.on_enabled = function(is_first_call)
-    mod:enable_all_hooks()
-end
+mod:hook_safe(LobbyItemsList, "populate_lobby_list", function (self)
+  for _, content in ipairs(self.item_list_widget.content.list_content) do
+    if content.lobby_data then
+        content.title_text = string.format("[%s] %s", content.lobby_data.country_code, content.title_text)
+    end
+  end
+end)
