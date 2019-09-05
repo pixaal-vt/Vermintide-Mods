7dbbe4ef85f3d3f5a55731ade03e2fbe27954049
pixaal-vt <pixaal-vt@protonmail.com>
2019-09-05 11:00:34 +0200

    Update widget definitions

--- Crosshair Kill Confirmation/scripts/mods/Crosshair Kill Confirmation/Crosshair Kill Confirmation_localization.lua
+++ Crosshair Kill Confirmation/scripts/mods/Crosshair Kill Confirmation/Crosshair Kill Confirmation_localization.lua
@@ -2,22 +2,71 @@ return {
 	mod_description = {
 		en = "Crosshair Kill Confirmation description"
 	},
-	example_checkbox_option_name = {
-		en = "Example Checkbox Option"
+	
+	duration = {
+		en = "Duration"
 	},
-	example_checkbox_option_tooltip = {
-		en = "Example Checkbox Option description."
+	duration_description = {
+		en = "How long the indicator is shown for when killing an enemy (in seconds).\nDefault: 1"
 	},
---[[
-	some_other_text_id = {
-		en = "Translation",        -- English
-		fr = "Translation",        -- French
-		de = "Translation",        -- German
-		es = "Translation",        -- Spanish
-		ru = "Translation",        -- Russian
-		it = "Translation",        -- Italian
-		pl = "Translation",        -- Polish
-		["br-pt"] = "Translation", -- Portuguese-Brazil
+	size = {
+		en = "Size"
 	},
---]]
+	size_description = {
+		en = "How big the kill indicator is.\nDefault: 52"
+	},
+	pop = {
+		en = "Pop effect"
+	},
+	pop_description = {
+		en = "For elites, sepecials and monsters, the kill indicator grows a bit as it fades out, making a sort of \"pop\" effect. This controls how much it grows by.\nDefault: 2.2"
+	},
+	crosshair_normal = {
+		en = "Normal kills"
+	},
+	crosshair_normal_description = {
+		en = "Choose the appearance of the kill indicator crosshair for regular enemies.\nDefault: Ex"
+	},
+	crosshair_special = {
+		en = "Special kills"
+	},
+	crosshair_special_description = {
+		en = "Choose the appearance of the kill indicator crosshair for specials.\nDefault: Circle"
+	},
+	crosshair_elite = {
+		en = "Elite kills"
+	},
+	crosshair_elite_description = {
+		en = "Choose the appearance of the kill indicator crosshair for stronger/elite enemies.\nDefault: Triangle"
+	},
+	crosshair_boss = {
+		en = "Boss kills"
+	},
+	crosshair_boss_description = {
+		en = "Choose the appearance of the kill indicator crosshair for monsters and lords.\nDefault: Square"
+	},
+	option_circle = {
+		en = "Circle"
+	},
+	option_dot = {
+		en = "Dot"
+	},
+	option_ex = {
+		en = "Ex"
+	},
+	option_plus = {
+		en = "Plus"
+	},
+	option_square = {
+		en = "Square"
+	},
+	option_triangle = {
+		en = "Triangle"
+	},
+	option_heart = {
+		en = "Heart"
+	},
+	option_none = {
+		en = "None"
+	},
 }
